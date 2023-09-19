ac63e0ce4de3bc3192227708ea13be12dd7d0cbb
QuocDuy <nguyendqduy@gmail.com>
2023-09-19 09:29:34 +0700

    duydeptrai

---
+++ src/main/java/com/example/controllers/LoginController.java
@@ -0,0 +1,44 @@
+package com.example.controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.example.models.User;
+
+
+public class LoginController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String url = "/index.html";
+
+        String action = request.getParameter("action");
+
+        if(action == null)
+            action="join";
+
+        if(action.equals("join"))
+            url = "/index.html";
+
+        else if (action.equals("add")){
+            String firstname =  request.getParameter("firstName");
+            String lastname =  request.getParameter("lastName");
+            String email =  request.getParameter("email");
+            User user = new User(lastname, firstname, email);
+
+            request.setAttribute("user", user);
+            url = "/login.jsp";
+        }
+
+        getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        doPost(request, response);
+    }
+}
