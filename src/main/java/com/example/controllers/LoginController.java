b1e8df7d3e93189714b20c64060132166a71f1f7
QuocDuy <nguyendqduy@gmail.com>
2023-09-19 21:52:01 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/artifacts/HelloWebApp_Web_exploded/META-INF/MANIFEST.MF
    #	target/HelloWebApp-1.0-SNAPSHOT.war

--- src/main/java/com/example/controllers/LoginController.java
+++ src/main/java/com/example/controllers/LoginController.java
@@ -13,25 +13,17 @@ public class LoginController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String url = "/index.html";
 
-        String action = request.getParameter("action");
 
-        if (action == null)
-            action = "join";
 
-        if (action.equals("join"))
-            url = "/index.html";
-
-        else if (action.equals("add")) {
             String firstname = request.getParameter("firstName");
             String lastname = request.getParameter("lastName");
             String email = request.getParameter("email");
             User user = new User(lastname, firstname, email);
 
             request.setAttribute("user", user);
-            url = "/login.jsp";
-        }
+            String url = "/login.jsp";
+
 
         getServletContext().getRequestDispatcher(url).forward(request, response);
     }
